e41cf752b408e939323e52faf0c57e37a597a416
miltonlopezjr <miltonathm@hotmail.com>
2018-09-02 12:17:08 -0400

    search with genre

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -7,14 +7,24 @@ const api_key = process.env.API_KEY;
 //Return requests to the client
 module.exports = {
   getSearch: (req, res) => {
-    res.send('hi getSearch');
-    // get the search genre     
-
-    // use this endpoint to search for movies by genres, you will need an API key
-
-    // https://api.themoviedb.org/3/discover/movie
-
-    // and sort them by horrible votes using the search parameters in the API
+    let genre = req.query.genre;
+    axios.get('https://api.themoviedb.org/3/discover/movie', {
+      params: {
+        api_key : api_key,
+        language : 'en-US',
+        sort_by : 'vote_count.asc',
+        include_adult : 'false',
+        include_video : 'false',
+        page : '1',
+        with_genres : genre
+      }
+    })
+      .then(({data}) => {
+        res.send(data);
+      })
+      .catch((error) => {
+        res.send(error);
+      });
   },
   getGenres: (req, res) => {
     // use this endpoint, which will also require your API key: https://api.themoviedb.org/3/genre/movie/list
